0372d830d857df967a9486414a058d77ef7b6c86
20231640 <20231640@novaims.unl.pt>
2024-11-21 15:29:43 +0000

    first commit 5

---
+++ shad.py
@@ -0,0 +1,44 @@
+import pygame
+from config import*
+from utils import*
+from interface import under_construction
+
+def shed(player):
+    #set up of the background and screen
+    background = pygame.image.load("farm.png")
+    background = pygame.transform.scale(background, resolution)
+    screen= pygame.display.set_mode(resolution)
+    clock = pygame.time.Clock
+
+    #set player's position to the left of the screen
+    player.rect.left = 0
+
+    special_ares = pygame.Rect(530, 30, 140, 140)
+
+    running= True
+    while running:
+        clock.tick(fps)
+        screen.blit(background,(0,0))
+
+        #Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        #uPDATE THEIR POSITION
+        player.update()
+
+        #detect if the user walks in the special area (house)
+        if special_area.colliderect(player.rect):
+            under_construction()
+            #change player position to avoid an infinite loop
+            player.rect.top = 200
+            player.rect.left = 560
+
+        #allow a player to return to the previous screen
+        if player.rect.left <= 0:
+            #position the player to the right of screen
+            player.rect.left = width-player.rect.width
+            return "main" # we want to switch to the main game
+
+        #draw player
